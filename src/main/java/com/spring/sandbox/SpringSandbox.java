edb04275d828cbebf88e07b769024889b858606a
jdavis31 <jdavis@captechconsulting.com>
2022-02-24 10:50:29 -0600

    Initial commit

---
+++ src/main/java/com/spring/sandbox/SpringSandbox.java
@@ -0,0 +1,13 @@
+package com.spring.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandbox {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSandbox.class, args);
+	}
+
+}
