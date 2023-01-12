c5e26cc4c952a121a9fa35fd8e35dbe7af4d9ed5
jdavis31 <jdavis@captechconsulting.com>
2023-01-12 11:42:10 -0600

    Added getters for public and secured routes

--- src/main/java/com/judell/constants/RouteConstants.java
+++ src/main/java/com/judell/constants/RouteConstants.java
@@ -1,12 +1,17 @@
 package com.judell.constants;
 
-import lombok.Getter;
-
-@Getter
 public class RouteConstants {
     private final static String[] SECURED_ROUTES = {};
 
     private final static String[] PUBLIC_ROUTES = {};
 
     public final static String HEALTH = EnvironmentConstants.BASE_URL + "/health";
+
+    public static String[] getPublicRoutes() {
+        return PUBLIC_ROUTES;
+    }
+
+    public static String[] getSecuredRoutes() {
+        return SECURED_ROUTES;
+    }
 }
